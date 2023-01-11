4e42c816374067d49c31e5159e5b41e5aafb45f1
StefanRacic <86464790+racicstefan@users.noreply.github.com>
2023-01-11 17:05:48 +0100

    Remove index from services

--- src/pages/Exercises/Exercises.js
+++ src/pages/Exercises/Exercises.js
@@ -1,15 +1,12 @@
 import Grid from '@mui/material/Unstable_Grid2/Grid2';
 import { Container } from '@mui/system';
 import React, { useEffect, useState } from 'react';
-import { getExercies } from '../../services';
 import Exercise from './Exercise';
 import AddExercise from '../../components/forms/AddExercise';
 
 const Exercises = () => {
 	const [exercises, setExercises] = useState([]);
-	useEffect(() => {
-		getExercies().then((res) => setExercises(res.data));
-	}, []);
+	useEffect(() => {}, []);
 	return (
 		<React.Fragment>
 			<Container sx={{ p: 10 }}>
--- src/services/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const getPrograms = () => {
-	return axios.get('https://localhost:7097/Programs/GetAllPrograms');
-};
-
-export const getExercies = () => {
-	return axios.get('https://localhost:7097/Exercises/GetAllExercises');
-};
